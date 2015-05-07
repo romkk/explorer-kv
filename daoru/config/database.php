50cbb2d757feddfb2dd3fbebddac8eb1d4806537
litianzhao <litianzhao@tangpool.com>
2015-05-07 12:12:58 +0800

    修复了 database 配置中遗漏数据库名的问题

--- daoru/config/database.php
+++ daoru/config/database.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'host' => getenv('DATABASE_HOST'),
+    'name' => getenv('BitcoinExplorerDB'),
     'user' => getenv('DATABASE_USER'),
     'pass' => getenv('DATABASE_PASS'),
     'port' => getenv('DATABASE_PORT'),

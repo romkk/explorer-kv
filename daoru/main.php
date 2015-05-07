11397f206b5b432d03033baf1fb62a7fa7d10851
litianzhao <litianzhao@tangpool.com>
2015-05-07 12:14:26 +0800

    将数据库初始化过程移动到单独的文件中

--- daoru/main.php
+++ daoru/main.php
@@ -1,31 +1,14 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+
+# load dotenv
 Dotenv::load(__DIR__);
+
+# path
 Config::init(require __DIR__ . '/bootstrap/paths.php');
-Log::init();
 
-use Illuminate\Database\Capsule\Manager as Capsule;
+# monolog
+Log::init(new Monolog\Handler\RotatingFileHandler(Config::get('app.log_path') . '/daoru.log', 3));
 
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'database',
-    'username'  => 'root',
-    'password'  => 'password',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-
-// Set the event dispatcher used by Eloquent models... (optional)
-use Illuminate\Events\Dispatcher;
-use Illuminate\Container\Container;
-$capsule->setEventDispatcher(new Dispatcher(new Container));
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
+# database
+require __DIR__ . '/bootstrap/database.php';

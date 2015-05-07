4b94b90ff5be25c87b9d01696c7f5507f2ef7cc8
litianzhao <tianzhao.li@bitmain.com>
2015-05-07 12:56:42 +0800

    改进了 bootstrap 对初始化流程的依赖

--- daoru/test/bootstrap.php
+++ daoru/test/bootstrap.php
@@ -1,10 +1,2 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
-
-Dotenv::load(__DIR__);
-
-Config::init(require __DIR__ . '/../bootstrap/paths.php');
-
-Log::init(new Monolog\Handler\RotatingFileHandler(Config::get('app.log_path') . '/daoru.log', 3));
-
-require __DIR__ . '/../bootstrap/database.php';
+require __DIR__ . '/../main.php';

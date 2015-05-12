aa49ff5ffa6fdaea75a44df6d9f3d6fc475b7eb0
litianzhao <litianzhao@tangpool.com>
2015-05-12 22:41:20 +0800

    修改 rawblock 的模型定义

--- daoru/model/RawBlock.php
+++ daoru/model/RawBlock.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 class RawBlock extends Model {
     protected $table = '0_raw_blocks';
 
+    protected $fillable = ['block_hash', 'block_height', 'chain_id', 'hex', 'created_at'];
+    public $timestamps = false;
+
     public static function findLatestBlock() {
         return static::orderBy('id', 'desc')
             ->first(['id', 'block_hash', 'block_height', 'chain_id', 'created_at']);

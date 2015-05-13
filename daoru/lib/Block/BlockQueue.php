5405a448d2451deaf27f1e272a75246c905bc1cb
litianzhao <litianzhao@tangpool.com>
2015-05-13 17:58:00 +0800

    完善了 blockqueue 的功能

--- daoru/lib/Block/BlockQueue.php
+++ daoru/lib/Block/BlockQueue.php
@@ -1,6 +1,123 @@
 <?php
 
+use Illuminate\Support\Collection;
+
 class BlockQueue {
+
+    protected $queue = null;
+    protected $queueLength = null;
+
+    public function __construct(Collection $localBlocks, $queueLength = 50) {
+        foreach ($localBlocks as $blk) {
+            if (!$blk instanceof Block) {
+                throw new Exception('TypeError: BlockQueue construct need array elements of Block type.');
+            }
+        }
+        $this->queue = new Collection($localBlocks);
+        $this->queueLength = $queueLength;
+    }
+
+    public function push(Block $block) {
+        $this->queue->push($block);
+        if ($this->length() > $this->queueLength) {
+            $this->queue->shift();
+        }
+    }
+
+    public function length() {
+        return count($this->queue);
+    }
+
+    public function getBlock($offset = -1) {
+        if ($offset < 0) {
+            $offset = $this->length() + $offset;
+        }
+        return $offset < 0 ? null : $this->queue->offsetGet($offset);
+    }
+
+    public function rollback() {
+        //TODO
+    }
+
+    public function diff(Block $remote) {
+        $block = $this->getBlock() ?: new Block(null, null, -1, 1);
+        return $block->getHeight() < $remote->getHeight() ||
+            $block->getHeight() === $remote->getHeight() && $block->getHash() !== $remote->getHash();
+    }
+
+    //TODO 处理高度相等，但哈希不同的情况
+    public function digest(Block $remote) {
+        $bitcoinClient = Bitcoin::make();
+        $rollbackOffset = 0;
+
+        if ($this->length() === 0) {        //第一次初始化，无块
+            return [$remote, new Collection()];
+        }
+
+        while ($rollbackOffset < $this->length()) {
+            $localPointer = $this->getBlock(-$rollbackOffset - 1);
+            $currentHeight = $remote->getHeight() -  $rollbackOffset;
+            if ($rollbackOffset === 0) {
+                $remotePointer = $remote;
+            }  else {
+                $hash = $bitcoinClient->getblockhash($currentHeight);
+                $detail = $bitcoinClient->bm_get_block_detail($hash);
+                $remotePointer = new Block($detail['hash'], $detail['previousblockhash'], $currentHeight, 1);
+                $remotePointer->setTxs(array_map(function($tx) use ($remotePointer) {
+                    $t = new Tx($remotePointer, $tx['hash']);
+                    $t->setHex($tx['rawhex']);
+                    return $t;
+                }, $detail['tx']));
+            }
+
+            if ($localPointer->getHash() === $remotePointer->getPrevHash()) {      //命中 block，计算出 orphan block
+                $orphanBlocks = $this->queue->splice($this->length() - $rollbackOffset, $rollbackOffset);
+                $this->push($remotePointer);
+                return [$remotePointer, $orphanBlocks];
+            } else {        // miss
+                $rollbackOffset++;
+            }
+        }
+
+        if (getenv('ENV') === 'test') {
+            return false;
+        } else {
+            Log::error(sprintf('diff offset 超过预设的 %d，进程退出。', $this->queueLength));
+            exit(1);
+        }
+
+    }
+
+    public static function make() {
+        $blocks = RawBlock::where('chain_id', 0)
+            ->orderBy('block_height', 'desc')
+            ->take(50)
+            ->get(['id', 'block_hash', 'block_height', 'chain_id', 'created_at'])
+            ->map(function (RawBlock $block) {
+
+                $detail = Bitcoin::make()->bm_get_block_detail($block->block_hash);
+
+                $block = new Block($block->block_hash, $detail['previousblockhash'], $block->block_height, 1);
+
+                $block->setTxs(array_map(function($tx) use ($block) {
+                    $t = new Tx($block, $tx['hash']);
+                    $t->setHex($tx['rawhex']);
+                    return $t;
+                }, $detail['tx']));
+
+                return $block;
+            });
+
+        return new static($blocks);
+    }
+
+    public static function getRemoteBlockInfo() {
+        $i = Bitcoin::make()->bm_get_best_block();
+        return [
+            'block_height' => $i['height'],
+            'block_hash' => $i['hash'],
+        ];
+    }
 
     public static function getLocalBlockInfo() {
         $i = RawBlock::findLatestBlock();
@@ -13,13 +130,5 @@ class BlockQueue {
         }
 
         return $i->toArray();
-    }
-
-    public static function getRemoteBlockInfo() {
-        $i = Bitcoin::make()->bm_get_best_block();
-        return [
-            'block_height' => $i['height'],
-            'block_hash' => $i['hash'],
-        ];
     }
 }
---
+++ daoru/test/lib/Block/BlockQueueTest.php
@@ -0,0 +1,143 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+
+class BlockQueueTest extends ExplorerDatabaseTestCase {
+
+    public function testGetLocalFirstBlock() {
+        $this->assertNotNull($block = BlockQueue::getLocalBlockInfo());
+        $this->assertEquals(-1, $block['block_height']);
+        $this->assertNull($block['block_hash']);
+    }
+
+    public function testGetLatestLocalBlock() {
+        $this->tableInsert('0_raw_blocks', [
+            ['id' => 1, 'block_hash' => 'block_hash_1', 'block_height' => 0, 'chain_id' => 0, 'hex' => 'hex1', 'created_at' => Carbon::now()->toDateTimeString()],
+            ['id' => 2, 'block_hash' => 'block_hash_2', 'block_height' => 1, 'chain_id' => 0, 'hex' => 'hex2', 'created_at' => Carbon::now()->toDateTimeString()],
+        ]);
+
+        $this->assertNotNull($block = BlockQueue::getLocalBlockInfo());
+        $this->assertEquals(1, $block['block_height']);
+        $this->assertEquals(2, $block['id']);
+
+        $this->tableTruncate('0_raw_blocks');
+    }
+
+    public function testGetLatestRemoteBlock() {
+        $this->assertNotNull($block = BlockQueue::getRemoteBlockInfo());
+        $this->assertArrayHasKey('block_height', $block);
+        $this->assertArrayHasKey('block_hash', $block);
+        $this->assertArrayNotHasKey('hash', $block);
+        $this->assertArrayNotHasKey('height', $block);
+        $this->assertGreaterThan(0, $block['block_height']);
+        $this->assertGreaterThan(0, strlen($block['block_height']));
+    }
+
+    public function testMake() {
+        $queue = BlockQueue::make();
+        $this->assertEquals(0, $queue->length());
+
+        $this->tableInsert('0_raw_blocks', [
+            ['id' => 1, 'block_hash' => '000000000000226f7618566e70a2b5e020e29579b46743f05348427239bf41a1', 'block_height' => 300000, 'chain_id' => 0, 'hex' => 'hex1', 'created_at' => Carbon::now()->toDateTimeString()],
+            ['id' => 2, 'block_hash' => '0000000000004829474748f3d1bc8fcf893c88be255e6d7f571c548aff57abf4', 'block_height' => 300001, 'chain_id' => 0, 'hex' => 'hex2', 'created_at' => Carbon::now()->toDateTimeString()],
+        ]);
+
+        $queue = BlockQueue::make();
+        $this->assertEquals(2, $queue->length());
+
+        $this->tableTruncate('0_raw_blocks');
+    }
+
+    public function testPush() {
+        $queue = new BlockQueue(new Collection(), 1);
+        $this->assertEquals(0, $queue->length());
+
+        $queue->push(new Block('hash', 'prevhash', 0, 1));
+        $this->assertEquals(1, $queue->length());
+        $this->assertEquals('hash', $queue->getBlock()->getHash());
+
+        $queue->push(new Block('hash2', 'prevhash2', 1, 1));
+        $this->assertEquals(1, $queue->length());
+        $this->assertEquals('hash2', $queue->getBlock()->getHash());
+    }
+
+    public function testGetBlock() {
+        $queue = new BlockQueue(new Collection([
+            new Block('hash1', 'prevhash1', 0, 1),
+            new Block('hash2', 'prevhash2', 1, 1),
+        ]));
+
+        $this->assertEquals(1, $queue->getBlock()->getHeight());
+        $this->assertEquals(0, $queue->getBlock(-2)->getHeight());
+
+        $queue = new BlockQueue(new Collection());
+
+        $this->assertNull($queue->getBlock());
+    }
+
+    public function testDigestNoOrphan() {
+        $queue = new BlockQueue(new Collection([
+            new Block('hash1', 'prevhash1', 0, 1),
+            new Block('hash2', 'prevhash2', 1, 1),
+            new Block('hash3', 'prevhash3', 2, 1),
+        ]));
+
+        $remote = new Block('hash4', 'hash3', 3, 1);
+        list($remotePointer, $orphanBlocks) = $queue->digest($remote);
+        $this->assertEquals(0, count($orphanBlocks));
+        $this->assertEquals($remotePointer, $remote);
+    }
+
+    public function testDigestOrphanBlockFound() {
+        $queue = new BlockQueue(new Collection([
+            new Block('000000000000226f7618566e70a2b5e020e29579b46743f05348427239bf41a1', '00000000dfe970844d1bf983d0745f709368b5c66224837a17ed633f0dabd300', 300000, 1412899877),
+            new Block('fakehash', '000000000000226f7618566e70a2b5e020e29579b46743f05348427239bf41a1', 300001, 1412900707),
+            new Block('fakehash2', 'fakehash', 300002, 1412900867),
+        ]));
+
+        $remote = new Block('00000000ce07e1de4bec68ee5055259d5d64cdf4b23b0317135c1955296cab3d', '000000000000e6ae2516b4249f14f368dd57adc23d0f7c8a6c615f5cc0d50db8', 300003, 1412900867);
+        list($remotePointer, $orphanBlocks) = $queue->digest($remote);
+        $this->assertEquals(2, count($orphanBlocks));
+        $this->assertEquals('0000000000004829474748f3d1bc8fcf893c88be255e6d7f571c548aff57abf4', $remotePointer->getHash());
+    }
+
+    public function testDigestOrphanBlockNotFound() {
+        $queue = new BlockQueue(new Collection([
+            new Block('fakehashh', 'fakehhhh', 300000, 1412899877),
+            new Block('fakehash', 'fakehashh', 300001, 1412900707),
+            new Block('fakehash2', 'fakehash', 300002, 1412900867),
+        ]));
+
+        $remote = new Block('00000000ce07e1de4bec68ee5055259d5d64cdf4b23b0317135c1955296cab3d', '000000000000e6ae2516b4249f14f368dd57adc23d0f7c8a6c615f5cc0d50db8', 300003, 1412900867);
+        $this->assertFalse($queue->digest($remote));
+    }
+
+    public function testDiff() {
+        $queue = new BlockQueue(new Collection([
+            new Block('hash1', 'prevhash1', 0, 1),
+            new Block('hash2', 'prevhash2', 1, 1),
+            new Block('hash3', 'prevhash3', 2, 1),
+        ]));
+
+        $remote = new Block('hash4', 'hash3', 3, 1);
+        $this->assertTrue($queue->diff($remote));
+
+        $remote = new Block('hash3', 'prevhash3', 2, 1);
+        $this->assertFalse($queue->diff($remote));
+
+        $remote = new Block('hash4', 'hash3', 2, 1);
+        $this->assertTrue($queue->diff($remote));
+    }
+
+    /**
+     * Returns the test dataset.
+     *
+     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    protected function getDataSet() {
+        return new DbUnit_ArrayDataSet([
+            '0_raw_blocks' => [],
+        ]);
+    }
+}

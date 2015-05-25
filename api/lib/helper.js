4fc6f5e2e0836fd28250aaa774819e6caffbee31
litianzhao <litianzhao@tangpool.com>
2015-05-25 19:46:18 +0800

    添加了 address2hash160 方法，并添加了 helper 的单测

--- api/lib/helper.js
+++ api/lib/helper.js
@@ -1,8 +1,7 @@
 var sprintf = require('sprintf').sprintf;
 var mysql = require('./mysql');
+var bs58 = require('bs58');
 var log = require('debug')('api:lib:helper');
-
-
 
 module.exports ={
     constant: {
@@ -12,5 +11,10 @@ module.exports ={
     paramType(str) {
         str = String(str);
         return parseInt(str, 10).toString() === str ? this.constant.ID_IDENTIFIER : this.constant.HASH_IDENTIFIER;
+    },
+    addressToHash160(addr) {
+        addr = bs58.decode(addr);
+        addr = new Buffer(addr).toString('hex');
+        return addr.slice(2, addr.length - 8);
     }
 };

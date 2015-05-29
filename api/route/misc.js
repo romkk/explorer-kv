e5dfc99bd0d3947bd4374ee25e6807d1b2e85302
litianzhao <tianzhao.li@bitmian.com>
2015-05-29 12:54:27 +0800

    add unspent

--- api/route/misc.js
+++ api/route/misc.js
@@ -4,10 +4,64 @@ var helper = require('../lib/helper');
 var restify = require('restify');
 var _ = require('lodash');
 var Tx = require('../lib/Tx');
+var Address = require('../lib/Address');
+var validators = require('../lib/custom_validators');
+var sprintf = require('sprintf').sprintf;
+var assert = require('assert');
 
 module.exports = (server) => {
     server.get('/unspent', (req, res, next) => {
+        var err = validators.isValidAddressList(req.query.active);
+        if (err != null) {
+            return next(err);
+        }
 
+        var parts = req.params.active.trim().split('|');
+
+        var sql = `select height from 0_blocks
+                   where chain_id = 0
+                   order by block_id desc
+                   limit 1`;
+
+        mysql.pluck(sql, 'height')
+            .then(height => {
+                return Promise.all(parts.map(p => {
+                    var ret = {};
+                    return Address.make(p)
+                        .then(addr => {
+                            if (addr == null) {
+                                throw new restify.InvalidArgumentError(`Address not found: ${p}`);
+                            }
+                            var table = sprintf('address_unspent_outputs_%04d', addr.attrs.id % 10);
+                            var sql = `select tx_id, block_height, value
+                                 from ${table}
+                                 where address_id = ?`;
+                            return mysql.selectOne(sql, [addr.attrs.id]);
+                        })
+                        .then(row => {
+                            assert(row !== null, 'row can not be null');
+                            ret.value = row.value;
+                            ret.value_hex = row.value.toString(16);
+                            ret.confirmations = height - row.block_height + 1;
+
+                            // get tx
+                            return Tx.make(row.tx_id);
+                        })
+                        .then(tx => {
+                            assert(tx !== null, 'tx can not be null');
+                            ret.tx_hash = tx.attrs.hash;
+                            ret.tx_hash_big_endian = helper.toBigEndian(tx.attrs.hash);
+                            ret.tx_index = tx.attrs.tx_id;
+                            ret.tx_output_n = tx.attrs.outputs_count;
+
+                            return ret;
+                        });
+                }));
+            })
+            .then(ret => {
+                res.send(ret);
+                next();
+            }, next);
     });
 
     server.get('/unconfirmed-transactions', (req, res, next) => {
@@ -26,9 +80,9 @@ module.exports = (server) => {
                 return new Promise((resolve, reject) => {
                     (function loop() {
                         var sql = `select id, tx_hash, handle_type, block_height
-                               from ${table}
-                               order by id desc
-                               limit ${offset}, 100`;
+                                   from ${table}
+                                   order by id desc
+                                   limit ${offset}, 100`;
                         mysql.query(sql).then(rows => {
                             for (let r of rows) {
                                 if (r.handle_type == 1 && r.block_height == -1) {

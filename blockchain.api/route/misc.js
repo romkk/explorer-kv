db3e6552eb273177222ca9cc7a8b34ea006ce9a2
litianzhao <tianzhao.li@bitmian.com>
2015-06-09 15:39:34 +0800

    重构了 unconfirmed-transaction 接口

--- blockchain.api/route/misc.js
+++ blockchain.api/route/misc.js
@@ -10,6 +10,13 @@ var Block = require('../lib/block');
 var validators = require('../lib/custom_validators');
 
 module.exports = (server) => {
+    server.get('/unconfirmed-transactions', async (req, res, next) => {
+        var txHashList = await mysql.list(`select tx_hash from 0_unconfirmed_txs order by position asc`, ['tx_hash']);
+        var txs = await Tx.multiGrab(txHashList, !req.params.skipcache);
+        res.send(_.compact(txs));
+        next();
+    });
+
     /*
     server.get('/unspent', async (req, res, next) => {
         var err = validators.isValidAddressList(req.query.active);
@@ -52,61 +59,4 @@ module.exports = (server) => {
         next();
     });
     */
-
-    server.get('/unconfirmed-transactions', (req, res, next) => {
-        const PAGESIZE = 1000;
-
-        var sql = `show tables like 'txlogs_%'`;
-        mysql.query(sql)
-            .then(rows => {
-                if (rows.length == 0) {
-                    console.error('Txlogs Table not found !');
-                    throw new restify.ServiceUnavailableError('Service unavailable, please try later.');
-                }
-                return Object.values(rows[rows.length - 1])[0];
-            })
-            .then(table => {
-                var offset = 0;
-                var txs = [];
-                return new Promise((resolve, reject) => {
-                    (function loop() {
-                        var sql = `select id, tx_hash, handle_type, block_height
-                                   from ${table}
-                                   order by id desc
-                                   limit ${offset}, ${PAGESIZE}`;
-                        mysql.query(sql).then(rows => {
-                            for (let r of rows) {
-                                if (r.handle_type == 1 && r.block_height == -1) {
-                                    txs.push(r.tx_hash);
-                                } else {
-                                    return resolve(txs.reverse());
-                                }
-                            }
-
-                            offset += PAGESIZE;
-                            loop();
-                        });
-                    })();
-                });
-            })
-            .then(txHashs => {
-                return Promise.all(txHashs.map(hash => {
-                    return Tx.make(hash).then(tx => {
-                        if (tx == null) {
-                            return tx;
-                        }
-                        return tx.load();
-                    });
-                }));
-            })
-            .then(txs => {
-                res.send({
-                    txs: _.compact(txs)
-                });
-                next();
-            })
-            .catch(err => {
-                next(err);
-            });
-    });
 };

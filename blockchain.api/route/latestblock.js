9847c7fe0b60b5e89448c5cc151a8b0d9cd80a0d
litianzhao <tianzhao.li@bitmian.com>
2015-06-10 15:08:01 +0800

    修复 latestblock 获取错误的问题

--- blockchain.api/route/latestblock.js
+++ blockchain.api/route/latestblock.js
@@ -3,17 +3,17 @@ var Block = require('../lib/block');
 var log = require('debug')('api:route:latestblock');
 
 module.exports = (server) => {
-    server.get('/latestblock/', (req, res, next)=> {
-        Block.getLatestHeight()
-            .then(height => Block.grab(height, 0, 0, false, !req.params.skipcache))
-            .then(blk => {
-                res.send({
-                    height: blk.height,
-                    hash: blk.hash,
-                    time: blk.time,
-                    txIndexes: blk.tx
-                });
-                next();
-            });
+    server.get('/latestblock/', async (req, res, next)=> {
+        var latestHeight = await Block.getLatestHeight();
+        var [blk] = await Block.grabByHeight(latestHeight, !req.params.skipcache);
+
+        res.send({
+            height: blk.height,
+            hash: blk.hash,
+            time: blk.time,
+            txIndexes: blk.tx
+        });
+
+        next();
     });
 };

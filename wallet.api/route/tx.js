e45f05ee5fc911d1d95ca3d87c3f906fbbdc8193
litianzhao <tianzhao.li@bitmain.com>
2015-06-18 20:19:23 +0800

    添加了构造交易的初步版本

--- wallet.api/route/tx.js
+++ wallet.api/route/tx.js
@@ -1,10 +1,44 @@
 var restify = require('restify');
 var validate = require('../lib/valid_json');
-var request = require('request-promise');
-var config = require('config');
 var log = require('debug')('wallet:route:tx');
 var _ = require('lodash');
-var bitcore = require('bitcore');
+var blockData = require('../lib/block_data');
+
+// 获取 unspent，找出合适的 unspent
+async function getUnspentTxs(sentFrom, amount, offset) {
+    const limit = 200;
+    var aggregated = 0;
+    var aggregatedTxs = [];
+
+    while (aggregated <= amount) {
+        let unspentList = await blockData('/unspent', {     // if error, throw it
+            active: sentFrom.join('|'),
+            offset: offset,
+            limit: limit
+        });
+        unspentList.unspent_outputs.every(tx => {
+            aggregated += tx.value;
+            aggregatedTxs.push(tx);
+            offset++;
+            //log(`tx.value = ${tx.value}, offset = ${offset}`);
+            return aggregated <= amount;
+        });
+    }
+
+    return [offset, aggregated, aggregatedTxs];
+}
+
+// http://bitcoin.stackexchange.com/questions/1195/how-to-calculate-transaction-size-before-sending
+function estimateFee(txSize, amountAvaiable, feePerKB) {
+    var estimatedFee = Math.ceil(txSize / 1000) * feePerKB;
+
+    if (estimatedFee < amountAvaiable) {
+        txSize += 20 + 4 + 34 + 4;                // Safe upper bound for change address script size in bytes
+        estimatedFee = Math.ceil(txSize / 1000) * feePerKB;
+    }
+
+    return estimatedFee;
+}
 
 module.exports = server => {
     server.post('/tx', validate('tx'), async (req, res, next) => {
@@ -12,20 +46,48 @@ module.exports = server => {
             sentFrom = req.body.from,
             sentTo = req.body.to;
 
-        var endpoint = config.get('explorerEndpoint');
+        var totalSentAmount = _.sum(sentTo, 'amount');
 
+        // 检查余额是否足够
+        var totalUnspentAmount;
         try {
-            var response = await request({
-                baseUrl: endpoint,
-                uri: `/unspent?active=${sentFrom.join('|')}`
-            });
-
-            res.send(JSON.parse(response));
+            totalUnspentAmount = _.sum(await blockData('/multiaddr', {
+                active: sentFrom.join('|')
+            }), 'final_balance');
         } catch (err) {
-            log('request unspent error', err.message);
-            res.send(new restify.InternalServerError('Please try again later.'));
+            return next(err);
         }
 
+        if (totalUnspentAmount <= totalSentAmount) {    //等于时则不足以支付手续费
+            res.send({
+                success: false,
+                code: 'TxUnaffordable',
+                msg: 'You are so poor.'
+            });
+            return next();
+        }
+
+        let offset = 0, aggregated = 0, aggregatedTxs = [], txSize = 0, fee = 0;
+        let iter = 1;
+        do {
+            // 获得 unspent 列表
+            let [curOffset, curAggregated, curAggregatedTxs] = await getUnspentTxs(sentFrom, fee + totalSentAmount - aggregated, offset);
+            offset = curOffset;
+            aggregated += curAggregated;
+            aggregatedTxs.push.apply(aggregatedTxs, curAggregatedTxs);
+            // 计算手续费
+            txSize = 148 * aggregatedTxs.length       // input
+                + 34 * sentTo.length               // output
+                + 10;                              // fixed bytes
+            fee = estimateFee(txSize, aggregated, feePerKB);
+            log(`iter = ${iter++}, offset = ${offset}, fee = ${fee}, totalSentAmount = ${totalSentAmount}, aggregated = ${aggregated}, aggregatedTxs.length = ${aggregatedTxs.length}`);
+        } while (fee + totalSentAmount > aggregated);
+
+        res.send({
+            success: true,
+            fee: fee,
+            unspent_txs: aggregatedTxs
+        });
         next();
     });
 

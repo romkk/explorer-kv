9d3f2a5155d18fb05a834cd7399067ee276e7fa6
litianzhao <tianzhao.li@bitmain.com>
2015-09-18 13:19:20 +0800

    修复了发布脚本错误

--- wallet.api/rsync.sh
+++ wallet.api/rsync.sh
@@ -2,10 +2,10 @@
 
 BASE=$(cd "$(dirname "$0")"; pwd); cd "$BASE"
 
-rsync -av --exclude .env --exclude node_modules --exclude .git --progress ./ aliyun.explorer-stage:/work/Explorer/wallet.api.testnet3
+rsync -av --exclude .env --exclude node_modules --exclude .git --progress ./ aliyun.explorer-stage-01:/work/Explorer/wallet.api.testnet3
 remote_cmd="cd /work/Explorer/wallet.api.testnet3 && npm install --verbose"
 ssh aliyun.explorer-stage-01 "$remote_cmd"
 
-rsync -av --exclude .env --exclude node_modules --exclude .git --progress ./ aliyun.explorer-stage:/work/Explorer/wallet.api
-remote_cmd="cd /work/Explorer/wallet.api.testnet3 && npm install --verbose"
+rsync -av --exclude .env --exclude node_modules --exclude .git --progress ./ aliyun.explorer-stage-01:/work/Explorer/wallet.api
+remote_cmd="cd /work/Explorer/wallet.api && npm install --verbose"
 ssh aliyun.explorer-stage-01 "$remote_cmd"

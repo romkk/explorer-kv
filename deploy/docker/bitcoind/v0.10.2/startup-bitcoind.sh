75131ddee2d2fcecbfc103b4943e12b595109ad5
zhibiao.pan <zhibiao.pan@bitmain.com>
2015-12-29 14:18:27 +0800

    remove old files

--- deploy/docker/bitcoind/v0.10.2/startup-bitcoind.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-nohup supervise /root/supervise_bitcoind > /dev/null &
-nohup supervise /root/supervise_bitcoind_testnet3 > /dev/null &
